4a6dd82fb24373a23b82521818bf4896d299712b
Victor <victor.mateus.silva@hotmail.com>
2022-11-23 00:01:27 -0300

    Primeiro projeto

---
+++ src/primeiroProjeto/Program.java
@@ -0,0 +1,13 @@
+package primeiroProjeto;
+
+import java.util.Locale;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		System.out.println("hello world!");
+		
+	}
+
+}
